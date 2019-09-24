dc6800b52bb5bacd0acaa70ca11924420ff09dfc
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-09-24 17:31:00 +0900

    change app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
-
-    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@3.x/css/materialdesignicons.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}">
   </head>
   <body>
     <div id="app"></div>
